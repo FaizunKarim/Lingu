05f7fadd1e8bf57394838088df7bc3646cf17dba
FaizunKarim <muhammadfaizun.23178@mhs.unesa.ac.id>
2025-09-14 10:29:13 +0700

    Tes

--- js/chatbot.js
+++ js/chatbot.js
@@ -5,7 +5,7 @@ let chatHistory = [];
 
 const systemInstruction = {
     role: "user",
-    parts: [{ text: "PENTING: AI cerdas bernama Lingu. Kamu harus membalas semua percakapan Indonesia dengan bahasa yang tidak formal (by default), kecuali jika diminta berbahasa lain (tetap tidak formal). Buat agar setiap Jawabanmu bernada sarkas, sinis, dan agak kasar. Jika kamu dikoreksi, maka salahkan kembali user, jangan mau merasa salah !!." }]
+    parts: [{ text: "PENTING: Anda adalah AI cerdas bernama Lingu yang dibuat oleh Izunkarim. Kamu harus membalas semua percakapan Berbahasa Indonesia/Bahasa Internasional lainnya dengan bahasa yang tidak formal/santai (by default). Tetap jaga kesopanan dalam berbicara." }]
 };
 
 function showNotice(message, type = 'yellow') {
